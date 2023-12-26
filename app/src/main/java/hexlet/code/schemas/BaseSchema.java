bfffd2152d255c6de007c04208ab51cc7c1f9ade
Artur Lastovskiy <4372wow@gmail.com>
2023-12-26 18:59:17 +0300

    update project src

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,12 +1,12 @@
 package hexlet.code.schemas;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
 public class BaseSchema {
     private boolean isRequired;
-    private final Map<String, Predicate<Object>> requirements = new HashMap<>();
+    private final Map<String, Predicate<Object>> requirements = new LinkedHashMap<>();
     public final void setRequired(boolean required) {
         isRequired = required;
     }
@@ -21,10 +21,7 @@ public class BaseSchema {
     public final boolean isValid(final Object data) {
         if (!isRequired && !requirements.get("required").test(data)) {
             return true;
-        } else if (requirements.get("required").test(data)) {
-            return requirements.keySet().stream()
-                    .allMatch(k -> requirements.get(k).test(data));
         }
-        return false;
+        return requirements.keySet().stream().allMatch(k -> requirements.get(k).test(data));
     }
 }
